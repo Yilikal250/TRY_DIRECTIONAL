f945a4953c00402f30ae2c65ad9472c8764f8bd8
yilikal <yilikalt12@gmail.com>
2026-04-10 18:56:13 +0200

    Sectored the raw datats and created a way to access the sectored datas

--- Sectoring.m
+++
@@ -1,8 +0,0 @@
-
-fprintf('%-6s | %-20s\n', 'Sector', ' [Start - End)');
-fprintf('-------------------------------\n');
-for i = 1:length(compass_names)
-    low  = mod(center_points(i) - hw, 360);
-    high = mod(center_points(i) + hw, 360);
-    fprintf('%-6s | [%6.2f - %6.2f)\n', compass_names{i}, low, high);
-end
